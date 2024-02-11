b1c3dafb1e4ed181937279c319d8b0470caae2f1
Daniel L. Lu <daniel@lawrence.lu>
2024-02-11 12:52:36 -0800

    create a basic album

--- upload_blog.sh
+++ upload_blog.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # uploads a photo and copies the url to clipboard
-
 source "$(dirname "$0")/upload_photo.sh"
 
 # Check if a file path is provided
@@ -18,7 +17,19 @@ resized_link=$(upload_photo "$1" 1200)
 # Upload the full-size photo
 full_size_link=$(upload_photo "$1")
 
+exif_make=$(exiv2 "$1" -Pt -g Exif.Image.Make)
+exif_model=$(exiv2 "$1" -Pt -g Exif.Image.Model)
+exif_lens=$(exiv2 "$1" -Pt -g Exif.Photo.LensModel)
+exif_aperture=$(exiv2 "$1" -Pt -g Exif.Photo.ApertureValue
+exif_ss=$(exiv2 "$1" -Pt -g Exif.Photo.ShutterSpeedValue
+exif_iso=$(exiv2 "$1" -Pt -g Exif.Photo.ISOSpeedRatings)
+exif_fl=$(exiv2 "$1" -Pt -g Exif.Photo.FocalLength | awk '{print $1 "mm"}')
+
+# Assemble the description
+desc="$exif_make $exif_model with $exif_lens, $exif_fl. $exif_aperture, $exif_ss, ISO $exif_iso."
+
 # Output the sentence with both links
-output="pic $resized_link (full size: $full_size_link)"
+output="pic $resized_link : $desc (full size: $full_size_link)"
 
 echo $output | xclip -selection c
+echo $output
--- upload_clipboard.sh
+++ upload_clipboard.sh
@@ -2,8 +2,6 @@
 
 # uploads a photo and copies the url to clipboard
 
-source "$(dirname "$0")/upload_photo.sh"
-
 # Check for the presence of at least one argument
 if [ -z "$1" ]; then
     echo "Error: No file path provided."
@@ -15,16 +13,10 @@ echo "" | xclip -selection c
 # Call the upload function with the provided arguments
 if [ -n "$2" ]; then
     # If a resize argument is provided
-    output=$(upload_photo "$1" "$2")
+    output=$(python $(dirname "$0")/upload_photo.py "$1" "$2")
 else
     # If only the file path is provided
-    output=$(upload_photo "$1")
-fi
-
-# Check if a file path is provided
-if [ -z "$1" ]; then
-    echo "No file path provided"
-    exit 1
+    output=$(python $(dirname "$0")/upload_photo.py "$1")
 fi
 
 echo $output | xclip -selection c
