d6cf094a3d8b8a48caa262f86f2d3cd9a300c33c
Daniel Lu <daniel@lawrence.lu>
2025-01-01 21:25:23 -0800

    rename

--- gps.py
+++ gps.py
@@ -1,6 +1,7 @@
 from math import radians, sin, cos, sqrt, atan2
 import pyexiv2
 import yaml
+from typing import Optional, Tuple
 
 
 # Load banned areas from YAML file
@@ -33,8 +34,9 @@ def is_in_banned_area(lat, lon):
     return False
 
 
-# Function to remove GPS data if within banned area
-def remove_gps_if_banned(metadata: pyexiv2.ImageMetadata) -> bool:
+def lat_lon_from_metadata(
+    metadata: pyexiv2.ImageMetadata,
+) -> Optional[Tuple[float, float]]:
     gps_lat = metadata.get("Exif.GPSInfo.GPSLatitude")
     gps_lon = metadata.get("Exif.GPSInfo.GPSLongitude")
     gps_lat_ref = metadata.get("Exif.GPSInfo.GPSLatitudeRef")
@@ -49,12 +51,23 @@ def remove_gps_if_banned(metadata: pyexiv2.ImageMetadata) -> bool:
         lat_deg = gps_lat[0] + gps_lat[1] / 60 + gps_lat[2] / 3600
         lon_deg = gps_lon[0] + gps_lon[1] / 60 + gps_lon[2] / 3600
     else:
-        return False
+        return None
 
     if gps_lat_ref != "N":
         lat_deg = -lat_deg
     if gps_lon_ref != "E":
         lon_deg = -lon_deg
+
+    return (float(lat_deg), float(lon_deg))
+
+
+# Function to remove GPS data if within banned area
+def remove_gps_if_banned(metadata: pyexiv2.ImageMetadata) -> bool:
+    lat_lon = lat_lon_from_metadata(metadata)
+    if lat_lon is None:
+        return False
+    lat_deg, lon_deg = lat_lon
+
     if is_in_banned_area(lat_deg, lon_deg):
         # Clear GPS info from the metadata
         keys_to_delete = [key for key in metadata if key.startswith("Exif.GPSInfo")]
